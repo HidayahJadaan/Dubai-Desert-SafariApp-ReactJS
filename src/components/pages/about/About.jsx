d5568b799cafc2a83113ec82e8bb2ff25371a0f4
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-12-24 23:47:54 +0300

    Everything Is Done

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import './about.css'
 export default function About() {
   return (
-    <div>About</div>
+    <div className='about'>
+      <h1>About This App</h1>
+      <p>This app is all about booking safari desert tour in Dubai</p>
+
+      <p>
+        <strong>
+          Versio: 1.0.0
+        </strong>
+      </p>
+    </div>
   )
 }
--- src/components/pages/forms/Login.jsx
+++ src/components/pages/forms/Login.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./forms.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login() {
+
+  const [email, setEmail ] = useState("");
+  const [password, setPassword] = useState("");
+
+const formSubmitHandler = (e)=>{
+e.preventDefault();
+if(email.trim() === ""){
+  return toast.error("Email is required");
+}
+
+if(password.trim() === ""){
+  return toast.error("Password is required");
+}
+
+}
+
   return (
-    <div>Login Page</div>
-  )
+    <div className="form-wrapper" onSubmit={formSubmitHandler}>
+              <ToastContainer theme="colored" />
+
+      <form className="form">
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+        />
+        <button type="submit" className="form-btn">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
